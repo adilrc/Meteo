6d89f7a8f81007b7cd6db65274133994742bcb7b
Friendly Cashew <friendlycashew@icloud.com>
2022-11-06 22:30:31 -0800

    Run swift-format main

--- Meteo/Screens/Weather/WeatherContainerViewModel.swift
+++ Meteo/Screens/Weather/WeatherContainerViewModel.swift
@@ -11,24 +11,30 @@ import Foundation
 protocol WeatherContainerViewModelType: NSObjectProtocol {
   var selectedLocation: Location? { get }
   var weatherWrapperPublisher: Published<WeatherWrapper?>.Publisher { get }
-  
-  func reloadWeatherSummary(locAPI: LocationProviding, weatherAPI: OpenWeatherAPIWeatherProviding, force: Bool) async throws -> WeatherWrapper?
-  func reloadForecast(locAPI: LocationProviding, weatherAPI: OpenWeatherAPIWeatherProviding, force: Bool) async throws -> WeatherForecast?
+
+  func reloadWeatherSummary(
+    locAPI: LocationProviding, weatherAPI: OpenWeatherAPIWeatherProviding, force: Bool
+  ) async throws -> WeatherWrapper?
+  func reloadForecast(
+    locAPI: LocationProviding, weatherAPI: OpenWeatherAPIWeatherProviding, force: Bool
+  ) async throws -> WeatherForecast?
 }
 
 final class WeatherContainerViewModel: NSObject, WeatherContainerViewModelType, ObservableObject {
   var selectedLocation: Location?
-  
+
   init(selectedLocation: Location? = nil) {
     self.selectedLocation = selectedLocation
   }
-  
+
   private var lastRequestOlderThanFiveMinutes: Bool {
     guard let weatherWrapper else { return true }
-    return Date.now.timeIntervalSince(weatherWrapper.weatherSummary.lastUpdate) > 60*5
+    return Date.now.timeIntervalSince(weatherWrapper.weatherSummary.lastUpdate) > 60 * 5
   }
-  
-  private func currentLocation(_ locAPI: LocationProviding = LocationAPI.shared) async throws -> Location {
+
+  private func currentLocation(_ locAPI: LocationProviding = LocationAPI.shared) async throws
+    -> Location
+  {
     if let selectedLocation {
       // User has already selected a location
       return selectedLocation
@@ -42,31 +48,35 @@ final class WeatherContainerViewModel: NSObject, WeatherContainerViewModelType, 
   }
 
   @MainActor @discardableResult
-  func reloadWeatherSummary(locAPI: LocationProviding = LocationAPI.shared,
-              weatherAPI: OpenWeatherAPIWeatherProviding = OpenWeatherAPI.shared,
-              force: Bool = false) async throws -> WeatherWrapper? {
+  func reloadWeatherSummary(
+    locAPI: LocationProviding = LocationAPI.shared,
+    weatherAPI: OpenWeatherAPIWeatherProviding = OpenWeatherAPI.shared,
+    force: Bool = false
+  ) async throws -> WeatherWrapper? {
     guard lastRequestOlderThanFiveMinutes || force else { return nil }
-    
+
     let location = try await currentLocation(locAPI)
     logger.info("􀇕 Reloading weather summary for location: \(location.locality).")
     let weatherSummary = try await weatherAPI.weatherSummary(for: location)
-    
+
     let wrapper = WeatherWrapper(weatherSummary: weatherSummary, location: location)
     weatherWrapper = wrapper
     return wrapper
   }
-  
+
   @Published var weatherWrapper: WeatherWrapper?
   var weatherWrapperPublisher: Published<WeatherWrapper?>.Publisher { $weatherWrapper }
-  
+
   @MainActor @discardableResult
-  func reloadForecast(locAPI: LocationProviding, weatherAPI: OpenWeatherAPIWeatherProviding, force: Bool) async throws -> WeatherForecast? {
+  func reloadForecast(
+    locAPI: LocationProviding, weatherAPI: OpenWeatherAPIWeatherProviding, force: Bool
+  ) async throws -> WeatherForecast? {
     guard lastRequestOlderThanFiveMinutes || force else { return nil }
-    
+
     let location = try await currentLocation(locAPI)
     logger.info("􀇕 Reloading weather forecast for location: \(location.locality).")
     let forecast = try await weatherAPI.forecast(for: location)
-    
+
     return forecast
   }
 }
